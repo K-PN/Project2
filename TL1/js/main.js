3ce98315f44cc99eb6a332c78b1668d89cbb9012
KPgit4 <ngockhanh1116@gmail.com>
2022-03-01 18:19:41 +0700

    first commit

---
+++ TL1/js/main.js
@@ -0,0 +1,31 @@
+/*  ---------------------------------------------------
+    Template Name: Male Fashion
+    Description: Male Fashion - ecommerce teplate
+    Author: Colorib
+    Author URI: https://www.colorib.com/
+    Version: 1.0
+    Created: Colorib
+---------------------------------------------------------  */
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.filter__controls li').on('click', function () {
+            $('.filter__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.product__filter').length > 0) {
+            var containerEl = document.querySelector('.product__filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
+})(jQuery);
--- js/main.js
+++ js/main.js
@@ -2,6 +2,27 @@
 (function ($) {
     "use strict";
 
+    /*[ MY FIX ]
+    ===========================================================*/
+    
+    /*[ Preloader ]
+    ===========================================================*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.filter__controls li').on('click', function () {
+            $('.filter__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.product__filter').length > 0) {
+            var containerEl = document.querySelector('.product__filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
     /*[ Coming Soon ]
     ===========================================================*/
     var tooltipSpan = document.getElementById('tooltipp-span');
